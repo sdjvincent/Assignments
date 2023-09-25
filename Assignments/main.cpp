6de5af80ef953c5c669c71d0a4cc1ce71497512a
sdjvincent <vincent@avacadcam.com>
2023-09-25 07:50:24 +0100

    Initial Commit

---
+++ Assignments/main.cpp
@@ -0,0 +1,52 @@
+//
+//  main.cpp
+//  Assignments
+//
+//  Created by Simon Vincent on 25/09/2023.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+// 2.1
+
+//int main(int argc, const char * argv[]) {
+//
+//    const char *greeting;
+//    greeting = "Hello, World!";
+//    cout << greeting;
+//    return 0;
+//}
+
+// 2.2
+// for loop
+
+//int main(int argc, const char * argv[]) {
+//
+//    cout << "How many times would you like me to say hello?" << endl;
+//    
+//    int repeatGreeting {0};
+//    cin >> repeatGreeting;
+//    
+//    for (int i = 1; i <= repeatGreeting; ++i) 
+//    {
+//        cout <<  "Hello World! " << endl;
+//    }
+//    
+//    return 0;
+//
+
+
+// while loop
+
+int main(int argc, const char * argv[]) {
+
+    cout << "How many times would you like me to say hello?" << endl;
+    
+    
+    
+    return 0;
+}
+
